e2cb78945a552c3f84332e44197fd785769fca84
yanxi1231 <na.ya@northeastern.edu>
2026-03-11 00:07:55 -0400

    REACT Component for Dealers Page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
     # path for dealer reviews view
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
@@ -22,6 +22,5 @@ urlpatterns = [
     path(route='add_review', view=views.add_review, name='add_review'),
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
